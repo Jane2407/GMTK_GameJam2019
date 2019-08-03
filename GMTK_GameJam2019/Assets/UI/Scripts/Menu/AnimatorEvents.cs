5cd8f59fb970059b9fc2625106af8312a98754b1
Amanda <amandaduarte2012@gmail.com>
2019-08-02 22:58:28 -0400

    Set up UI Menus

---
+++ GMTK_GameJam2019/Assets/UI/Scripts/Menu/AnimatorEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimatorEvents : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    public bool disableOnce;
+
+
+    void PlaySound(AudioClip sound)
+    {
+        if (!disableOnce)
+        {
+            menuButtonController.audioSource.PlayOneShot(sound);
+        }
+        else
+        {
+            disableOnce = false;
+        }
+    }
+
+    public void ButtonPressed()
+    {
+        menuButtonController.ButtonPressed();
+    }
+}
---
+++ GMTK_GameJam2019/Assets/UI/Scripts/Menu/MenuButtonController.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public int index;
+    [SerializeField] int maxIndex;
+
+    public int activeMenuIndex;
+    [SerializeField] bool keyDown;
+
+    public AudioSource audioSource;
+
+    public List<GameObject> panels;
+
+    public Animator animBackground;
+
+    private void Start()
+    {
+        //audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            if (!keyDown)
+            {
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (index < maxIndex)
+                    {
+                        index++;
+                    }
+                    else
+                    {
+                        index = 1;
+                    }
+                }
+                else if(Input.GetAxis("Vertical") > 0)
+                {
+                    if(index > 0)
+                    {
+                        index--;
+                    }
+                    else
+                    {
+                        index = maxIndex;
+                    }
+                }
+
+                keyDown = true;
+            }
+        }
+        else
+        {
+            keyDown = false;
+        }
+    }
+
+    public void PointEnter(int thisIndex)
+    {
+        index = thisIndex;
+    }
+
+    public void PointExit()
+    {
+        index = 0;
+    }
+
+    public void ButtonPressed()
+    {
+        if (activeMenuIndex == 0)
+        {
+            switch (index)
+            {
+                case 4:
+                    OpenQuit();
+                    break;
+                case 3: 
+                    OpenCredits();
+                    break;
+                case 2: 
+                    OpenSettings();
+                    break;
+                case 1:
+                    animBackground.SetTrigger("Play");
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if(activeMenuIndex == 1)
+        {
+            if (index == 1)
+            {
+                OpenMain();
+            }
+        }
+        else if(activeMenuIndex == 2)
+        {
+            if (index == 1)
+            {
+                OpenMain();
+            }
+        }
+        else if (activeMenuIndex == 3)
+        {
+            switch (index)
+            {
+                case 2:
+                    CloseGame();
+                    break;
+                case 1:
+                    OpenMain();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void OpenMain()
+    {
+        panels[activeMenuIndex].SetActive(false);
+        panels[0].SetActive(true);
+        activeMenuIndex = 0;
+    }
+
+    void OpenCredits()
+    {
+        panels[activeMenuIndex].SetActive(false);
+        panels[2].SetActive(true);
+        activeMenuIndex = 2;
+    }
+
+    void OpenSettings()
+    {
+        panels[activeMenuIndex].SetActive(false);
+        panels[1].SetActive(true);
+        activeMenuIndex = 1;
+    }
+
+    void OpenQuit()
+    {
+        panels[activeMenuIndex].SetActive(false);
+        panels[3].SetActive(true);
+        activeMenuIndex = 3;
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
