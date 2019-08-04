3665abfabd0d9aa077fca33257e3b6cf23d94c74
Amanda <amandaduarte2012@gmail.com>
2019-08-04 11:53:43 -0400

    Configure Panels with Game Manager
    and added the In-Game panel with the button V indication (for the player)

--- GMTK_GameJam2019_Shaders/Assets/Scripts/GameManager.cs
+++ GMTK_GameJam2019_Shaders/Assets/Scripts/GameManager.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public string Level01;
+
+    public GameObject pausePanel;
+    public GameObject inGamePanel;
 
     public void NextLevel()
     {
@@ -15,6 +19,22 @@ public class GameManager : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        inGamePanel.SetActive(false);
+        pausePanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void UnPauseGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        inGamePanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        //blnPaused = false;
     }
 
 }
